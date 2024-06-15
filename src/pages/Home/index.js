8f131cc795b3b1febca6f20f05356e3aedea3591
kcleusio-dev <kcleusio.dev@gmail.com>
2024-06-15 17:56:42 +0100

    Adição de Perfil de Usuário

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -11,9 +11,19 @@ function App() {
         <img src={background} className="background" alt="background app" />
         <div className="info">
           <div>
-            <input name="usuario" placeholder="@username"/>
+            <input name="usuario" placeholder="@username" />
             <button>Buscar</button>
           </div>
+          <div className="perfil">
+            <img src="https://avatars.githubusercontent.com/u/144366835?v=4"
+              className="profile" alt="imagem de perfil"/>
+            <div>
+              <h3>Helder Daniel</h3>
+              <span>@kcleusio-dev</span>
+              <p>Descrição</p>
+            </div>
+          </div>
+          <hr />
         </div>
       </div>
     </div>
